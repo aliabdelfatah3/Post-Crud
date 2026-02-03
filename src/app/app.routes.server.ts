d1337f3563f08f67b2bf74aca2ba0be75e15dd5b
Ali Abdelfattah Saad <133794591+aliabdelfatah3@users.noreply.github.com>
2026-02-03 18:14:29 +0200

    solved prerender

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,17 @@
+// src/app/app.routes.server.ts
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    path: 'posts/edit/:id',
+    renderMode: RenderMode.Client, // بنقول لـ Angular: المسار ده سيبيه للمتصفح (Client) مكيش دعوة بيه في الـ Build
+  },
+  {
+    path: 'posts/:id',
+    renderMode: RenderMode.Client, // نفس الكلام لصفحة التفاصيل
+  },
+  {
+    path: '**', // أي مسار تاني (زي الـ List والـ Add)
+    renderMode: RenderMode.Prerender,
+  },
 ];
